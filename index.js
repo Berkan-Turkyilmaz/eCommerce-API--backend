af9419d30e79374e7de8b11e7906609ac05859cb
AmarbayarBold <abnabayar@gmail.com>
2024-07-30 10:50:11 +0200

    user part is done and works without any problem, some changes at order is made to function properly. may need update

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import express from "express";
-import { orderRouter } from "./routers/postRouter.js";
+import userRouter from "./routers/userRoutes.js";
+import sequelize from "./db/index.js";
+//
+//import { orderRouter } from "./routers/orderRoutes.js";
 
 const app = express();
-const port = 5000;
+const port = 4000;
 
-app.use(express.jason());
+app.use(express.json());
+app.use("/users", userRouter);
 
-app.use("/orders", orderRouter);
+//
+//app.use("/orders", orderRouter);
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connected!");
+    console.log(`Server is running on port ${port}`);
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+});
+//app.listen(port, () => console.log(`Server is running on port ${port}`));
